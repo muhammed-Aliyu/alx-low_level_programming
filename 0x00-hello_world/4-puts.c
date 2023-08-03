5356ce59bffd862b808a8d9e9669e34c62ab9390
muhammed-Aliyu <maliyu805@gmail.com>
2023-08-03 01:06:31 -0700

    101-wildcmp.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main . A program that print a line function
+ * Return: 0 (SUCCESS)
+ */
+int main(void)
+{
+puts ("Programming is like building a multilingual puzzle");
+return 0;
+}
